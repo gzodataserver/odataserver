958eb092ed72da633c5d18597af1883762b84003
Jonas Colmsjö <jonas.colmsjo@gizur.com>
2015-01-19 13:54:38 +0100

    Added test helpers

---
+++ tests/helpers.js
@@ -0,0 +1,63 @@
+// helpers.js
+//------------------------------
+//
+// 2015-01-15, Jonas Colmsjö
+//
+//------------------------------
+//
+// Misc helpers fucntions
+//
+//
+// Using Google JavaScript Style Guide
+// http://google-styleguide.googlecode.com/svn/trunk/javascriptguide.xml
+//
+//------------------------------
+
+(function(moduleSelf, undefined) {
+
+  var h = {};
+
+  var http = require('http');
+  var h = require('../src/helpers.js');
+
+  var CONFIG = require('../src/config.js');
+  var log = new h.log0(CONFIG.testLoggerOptions);
+
+  //
+  // Helper for making http requests
+  // -------------------------------
+
+  h.httpRequest = function(options, input, done) {
+    var _data = '';
+    var req = http.request(options, function(res) {
+      log.debug('status code:' + res.statusCode + ', headers: ' +
+      JSON.stringify(res.headers));
+
+      res.setEncoding('utf8');
+
+      res.on('data', function(chunk) {
+        _data += chunk;
+      });
+
+      res.on('end', function() {
+        done(_data, res.statusCode);
+      });
+    });
+
+    req.on('error', function(e) {
+      log.log('problem with request: ' + e.message);
+    });
+
+    if (input !== null) {
+      req.write(input);
+    }
+
+    req.end();
+  };
+
+  // Exports
+  // =======
+
+  module.exports = h;
+
+})(this);
